6848e90a0db426f7c811b63273996d22d6ea52c6
Iury <iurymff70@gmail.com>
2026-03-05 14:16:45 -0300

    Adicionando exercicio 7

---
+++ dicionarios_e_funcoes_7.py
@@ -0,0 +1,103 @@
+loja_discos = {
+    "disco_01": {"titulo": "Is This It", "banda": "The Strokes", "genero": "Indie Rock", "gravadora": "RCA", "pais": "EUA", "ano": 2001},
+    "disco_02": {"titulo": "Room on Fire", "banda": "The Strokes", "genero": "Indie Rock", "gravadora": "RCA", "pais": "EUA", "ano": 2003},
+    "disco_03": {"titulo": "First Impressions of Earth", "banda": "The Strokes", "genero": "Indie Rock", "gravadora": "RCA", "pais": "EUA", "ano": 2006},
+    "disco_04": {"titulo": "Angles", "banda": "The Strokes", "genero": "Indie Rock", "gravadora": "RCA", "pais": "EUA", "ano": 2011},
+    "disco_05": {"titulo": "Comedown Machine", "banda": "The Strokes", "genero": "Indie Rock", "gravadora": "RCA", "pais": "EUA", "ano": 2013},
+    "disco_06": {"titulo": "The New Abnormal", "banda": "The Strokes", "genero": "Indie Rock", "gravadora": "Cult", "pais": "EUA", "ano": 2020},
+    "disco_07": {"titulo": "Whatever People Say I Am, That's What I'm Not", "banda": "Arctic Monkeys", "genero": "Indie Rock", "gravadora": "Domino", "pais": "Reino Unido", "ano": 2006},
+    "disco_08": {"titulo": "Favourite Worst Nightmare", "banda": "Arctic Monkeys", "genero": "Indie Rock", "gravadora": "Domino", "pais": "Reino Unido", "ano": 2007},
+    "disco_09": {"titulo": "Humbug", "banda": "Arctic Monkeys", "genero": "Indie Rock", "gravadora": "Domino", "pais": "Reino Unido", "ano": 2009},
+    "disco_10": {"titulo": "Suck It and See", "banda": "Arctic Monkeys", "genero": "Indie Rock", "gravadora": "Domino", "pais": "Reino Unido", "ano": 2011},
+    "disco_11": {"titulo": "AM", "banda": "Arctic Monkeys", "genero": "Indie Rock", "gravadora": "Domino", "pais": "Reino Unido", "ano": 2013},
+    "disco_12": {"titulo": "Tranquility Base Hotel & Casino", "banda": "Arctic Monkeys", "genero": "Psychedelic Pop", "gravadora": "Domino", "pais": "Reino Unido", "ano": 2018},
+    "disco_13": {"titulo": "The Car", "banda": "Arctic Monkeys", "genero": "Art Rock", "gravadora": "Domino", "pais": "Reino Unido", "ano": 2022},
+    "disco_14": {"titulo": "Gorillaz", "banda": "Gorillaz", "genero": "Alternative Rock", "gravadora": "Parlophone", "pais": "Reino Unido", "ano": 2001},
+    "disco_15": {"titulo": "Demon Days", "banda": "Gorillaz", "genero": "Alternative Rock", "gravadora": "Parlophone", "pais": "Reino Unido", "ano": 2005},
+    "disco_16": {"titulo": "Plastic Beach", "banda": "Gorillaz", "genero": "Art Pop", "gravadora": "Parlophone", "pais": "Reino Unido", "ano": 2010},
+    "disco_17": {"titulo": "The Fall", "banda": "Gorillaz", "genero": "Electronic", "gravadora": "Parlophone", "pais": "Reino Unido", "ano": 2010},
+    "disco_18": {"titulo": "Humanz", "banda": "Gorillaz", "genero": "Electronic", "gravadora": "Parlophone", "pais": "Reino Unido", "ano": 2017},
+    "disco_19": {"titulo": "The Now Now", "banda": "Gorillaz", "genero": "Synth-pop", "gravadora": "Parlophone", "pais": "Reino Unido", "ano": 2018},
+    "disco_20": {"titulo": "Song Machine, Season One", "banda": "Gorillaz", "genero": "Alternative Rock", "gravadora": "Parlophone", "pais": "Reino Unido", "ano": 2020},
+    "disco_21": {"titulo": "Cracker Island", "banda": "Gorillaz", "genero": "Synth-pop", "gravadora": "Parlophone", "pais": "Reino Unido", "ano": 2023},
+    "disco_22": {"titulo": "Brothers", "banda": "The Black Keys", "genero": "Blues Rock", "gravadora": "Nonesuch", "pais": "EUA", "ano": 2010},
+    "disco_23": {"titulo": "El Camino", "banda": "The Black Keys", "genero": "Garage Rock", "gravadora": "Nonesuch", "pais": "EUA", "ano": 2011},
+    "disco_24": {"titulo": "Turn Blue", "banda": "The Black Keys", "genero": "Psychedelic Rock", "gravadora": "Nonesuch", "pais": "EUA", "ano": 2014},
+    "disco_25": {"titulo": "Let's Rock", "banda": "The Black Keys", "genero": "Rock", "gravadora": "Easy Eye Sound", "pais": "EUA", "ano": 2019},
+    "disco_26": {"titulo": "Dropout Boogie", "banda": "The Black Keys", "genero": "Blues Rock", "gravadora": "Nonesuch", "pais": "EUA", "ano": 2022},
+    "disco_27": {"titulo": "Weezer (Blue Album)", "banda": "Weezer", "genero": "Power Pop", "gravadora": "Geffen", "pais": "EUA", "ano": 1994},
+    "disco_28": {"titulo": "Pinkerton", "banda": "Weezer", "genero": "Emo", "gravadora": "Geffen", "pais": "EUA", "ano": 1996},
+    "disco_34": {"titulo": "Songs for the Deaf", "banda": "Queens of the Stone Age", "genero": "Stoner Rock", "gravadora": "Interscope", "pais": "EUA", "ano": 2002},
+    "disco_35": {"titulo": "Lullabies to Paralyze", "banda": "Queens of the Stone Age", "genero": "Stoner Rock", "gravadora": "Interscope", "pais": "EUA", "ano": 2005},
+    "disco_58": {"titulo": "Bleach", "banda": "Nirvana", "genero": "Grunge", "gravadora": "Sub Pop", "pais": "EUA", "ano": 1989},
+    "disco_59": {"titulo": "Nevermind", "banda": "Nirvana", "genero": "Grunge", "gravadora": "DGC", "pais": "EUA", "ano": 1991},
+    "disco_60": {"titulo": "In Utero", "banda": "Nirvana", "genero": "Grunge", "gravadora": "DGC", "pais": "EUA", "ano": 1993},
+    "disco_77": {"titulo": "Speak & Spell", "banda": "Depeche Mode", "genero": "Synth-pop", "gravadora": "Mute", "pais": "Reino Unido", "ano": 1981},
+    "disco_78": {"titulo": "Music for the Masses", "banda": "Depeche Mode", "genero": "Synth-pop", "gravadora": "Mute", "pais": "Reino Unido", "ano": 1987},
+    "disco_79": {"titulo": "Violator", "banda": "Depeche Mode", "genero": "Synth-pop", "gravadora": "Mute", "pais": "Reino Unido", "ano": 1990},
+    "disco_83": {"titulo": "True Romance", "banda": "Charli XCX", "genero": "Electropop", "gravadora": "Asylum", "pais": "Reino Unido", "ano": 2013},
+    "disco_84": {"titulo": "Sucker", "banda": "Charli XCX", "genero": "Pop Punk", "gravadora": "Asylum", "pais": "Reino Unido", "ano": 2014},
+    "disco_89": {"titulo": "The Fame", "banda": "Lady Gaga", "genero": "Dance-pop", "gravadora": "Interscope", "pais": "EUA", "ano": 2008},
+    "disco_90": {"titulo": "The Fame Monster", "banda": "Lady Gaga", "genero": "Dance-pop", "gravadora": "Interscope", "pais": "EUA", "ano": 2009}
+}
+
+#pega todos os titulos antes de 2000
+def antes_2000(loja_discos, ano):
+    lista = []
+    for disco in loja_discos.values():
+        if disco["ano"] < ano:
+            lista.append(disco) #se eu guardar o disco inteiro
+    return lista
+
+#imprimir todos os discos de um genero
+def discos_genero(loja_discos, genero):
+    lista_genero = []
+    for disco in loja_discos.values():
+        if disco["genero"].lower() == genero.lower():
+            lista_genero.append(disco)
+    return lista_genero
+
+#imprimir tudo de qualquer banda
+def imprime_banda(loja_discos, banda):
+    lista_banda = []
+    for disco in loja_discos.values():
+        if disco["banda"].lower() == banda.lower():
+            lista_banda.append(disco)
+    return lista_banda
+
+#imprime tudo de qualquer gravadora
+def imprime_gravadora(loja_discos, gravadora):
+    lista_gravadora = []
+    for disco in loja_discos.values():
+        if disco["gravadora"].lower() == gravadora.lower():
+            lista_gravadora.append(disco)
+    return lista_gravadora
+
+print("=========================")
+
+lista_2000 = antes_2000(loja_discos, 2000)
+print("Albuns antes dos anos 2000")
+for disco in lista_2000:
+    #consigo imprimir o titulo e o ano
+    print(f"Título: {disco['titulo']} | Ano: {disco['ano']}")
+
+
+print("=========================")
+
+#imprimindo todos os discos de um genero
+print("Segue abaixo todos os discos do genero: ")
+lista_genero = discos_genero(loja_discos, "indie rock")
+for disco in lista_genero:
+    print(f"Título: {disco['titulo']} | Artista: {disco['banda']}")
+
+print("==========================")
+print("Imprimindo os discos da banda escolhida: ")
+lista_banda = imprime_banda(loja_discos, "arctic monkeys")
+for disco in lista_banda:
+    print(f"Banda: {disco["banda"]} | Titulo: {disco["titulo"]}")
+
+print("==========================")
+print("Imprimindo os discos da gravadora")
+lista_gravadora = imprime_gravadora(loja_discos, "RCA")
+for disco in lista_gravadora:
+    print(f"Disco: {disco["titulo"]} | Gravadora {disco["gravadora"]}")
