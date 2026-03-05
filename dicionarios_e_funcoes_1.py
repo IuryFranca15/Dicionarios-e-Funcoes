b1aeea0194d2c6b28471661f2790e78da52a1c0f
Iury França <112043211+IuryFranca15@users.noreply.github.com>
2026-03-05 12:39:15 -0300

    Adicionando o desconto

--- dicionarios_e_funcoes_1.py
+++ dicionarios_e_funcoes_1.py
@@ -10,7 +10,7 @@ skate_pecas = {
 
 def calcular_combo(skate_pecas, item1, item2): #passa o dic, e os 2 itens
     if item1 in skate_pecas and item2 in skate_pecas:
-        combo = skate_pecas[item1] + skate_pecas[item2] #acessa o valor assim
+        combo = (skate_pecas[item1] + skate_pecas[item2])*0.85 #acessa o valor assim
         return f"Combo de {item1} e {item2} com 15% OFF: R$ {combo:.2f}"
     else:
         return f"Algum dos itens não está na lista"
